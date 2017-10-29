f6e26030caa1aa7f174a3775737466a4ff418e8c
hudaokeji <github@hudaokeji.com>
2017-10-29 10:57:07 +0900

    add custom config

---
+++ config/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  syncModel : "exist",
+  // registry url name
+  registryHost: '127.0.0.1:7001',
+
+  admins: {
+    // name: email
+    hudao: 'hudao@github.com'
+  },
+
+  sourceNpmRegistryIsCNpm: false
+};
